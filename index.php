0d127cb4fd1cfabdc05c4d7169bd339cacbba177
Melissa <melissa.fava@aluno.senai.br>
2025-11-27 11:48:58 -0300

    correcão

--- index.php
+++ index.php
@@ -14,35 +14,35 @@ $links = [
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>BioLink da <?php echo $nome; ?></title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>BioLink da <?php echo $nome; ?></title>
 
-</head>
-<body>
-    <div class="container">
-        <img src="<?php echo $imagem; ?>" alt="Foto de Perfil" class="avatar">
+    </head>
+    <body>
+        <div class="container">
+            <img src="<?php echo $imagem; ?>" alt="Foto de Perfil" class="avatar">
 
-        <h1> <?php echo $nome; ?> </h1>
-        <p> <?php echo $bio; ?> </p>
+            <h1> <?php echo $nome; ?> </h1>
+            <p> <?php echo $bio; ?> </p>
 
-        <div class="lista-links">
-            <?php
-                foreach ($links as $texto => $url) {
-                    $destaque = "";
-                    // DESAFIO: Se o texto for "WhatsApp" (ou outro que você queira), mude a variável $classe_extra para "destaque". Escreva o IF aqui:
-                    if ($texto == "Instagram") {
-                        $destaque = "destaque";
+            <div class="lista-links">
+                <?php
+                    foreach ($links as $texto => $url) {
+                        $destaque = "";
+                        // DESAFIO: Se o texto for "WhatsApp" (ou outro que você queira), mude a variável $classe_extra para "destaque". Escreva o IF aqui:
+                        if ($texto == "Instagram") {
+                            $destaque = "destaque";
+                        }
+                        // O PHP constrói o botão HTML
+                        echo "<a href='$url' class='btn $destaque' target='_blank'>
+                            $texto
+                        </a>";
                     }
-                    // O PHP constrói o botão HTML
-                    echo "<a href='$url' class='btn $destaque' target='_blank'>
-                        $texto
-                    </a>";
-                }
-            ?>
+                ?>
+            </div>
         </div>
-    </div>
-</body>
+    </body>
 </html>
